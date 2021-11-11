f6014009b16108b542679a30802bfa0a69abe924
renanimpacta <santana.renan@outlook.com>
2021-11-11 01:29:26 -0300

    Atualização deploy

--- backend/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ backend/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,13 +1,21 @@
 package org.generation.blogPessoal.model;
 
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import io.swagger.annotations.ApiModelProperty;
 
@@ -20,19 +28,42 @@ public class Usuario {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
-	@NotNull
+	@NotNull(message = "O atributo nome é obrigatório")
+	@NotBlank(message = "O atributo nome é Obrigatório!")
 	@Size(min = 5, max = 100)
 	private String nome;
 	
 	@ApiModelProperty(example = "email@email.com.br")
-	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@NotNull(message = "O atributo usuário é obrigatório")
+	@NotBlank(message = "O atributo Usuário é Obrigatório!")
 	@Email(message = "O atributo Usuário deve ser um email válido!")
 	@Size(min = 5, max = 100)
 	private String usuario;
 	
-	@NotNull
+	@NotNull(message = "O atributo nome é obrigatório")
+	@NotBlank(message = "O atributo senha é Obrigatório!")
 	@Size(min = 5, max = 100)
 	private String senha;
+	
+	private String foto;
+	
+	private String tipo;
+	
+	@OneToMany(mappedBy = "usuario", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List<Postagem> postagem;
+	
+	// Primeiro método Construtor - Com os atributos
+	public Usuario(long id, String nome, String usuario, String senha) {
+			this.id = id;
+			this.nome = nome;
+			this.usuario = usuario;
+			this.senha = senha;
+			}
+			
+	// Segundo método Construtor - Sem os atributos
+	public Usuario() { }
+
 
 	
 	//encapsulamento
@@ -66,6 +97,30 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 	
 
