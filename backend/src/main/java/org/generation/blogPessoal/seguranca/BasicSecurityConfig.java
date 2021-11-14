7ae80d18d2c8d0ddedc26dca68a2acdac41e1b55
renanimpacta <santana.renan@outlook.com>
2021-11-14 19:41:15 -0300

    Atualização BasecSecuriyConfig

--- backend/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
+++ backend/src/main/java/org/generation/blogPessoal/seguranca/BasicSecurityConfig.java
@@ -36,8 +36,11 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
+		.antMatchers("/**").permitAll()
 		.antMatchers("/usuarios/logar").permitAll()
 		.antMatchers("/usuarios/cadastrar").permitAll()
+		.antMatchers(HttpMethod.GET ,"/postagens").permitAll()
+        .antMatchers(HttpMethod.GET ,"/tema").permitAll()
 		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
